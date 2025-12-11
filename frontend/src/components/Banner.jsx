138b41f948cc592c0b6210205054e1e06ef49be0
huywb <pthhuy1306@gmail.com>
2025-12-11 16:59:39 +0700

    Banner Section

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,10 @@
+import banner from '../../public/featured.png'
+const Banner = () => {
+  return (
+    <div>
+        <img src={banner} alt="logo banner" />
+    </div>
+  )
+}
+
+export default Banner
