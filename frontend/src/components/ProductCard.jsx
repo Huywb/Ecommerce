72fb66086bcbee2aa38a25dbf1d3941dadffcfd6
huywb <pthhuy1306@gmail.com>
2025-12-12 16:39:57 +0700

    ProductCard component

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import { IoCartOutline } from "react-icons/io5";
+import ProductDetail from "./ProductDetail";
 const ProductCard = (item) => {
     const [selected,setSelected] = useState(null)
 
     return (
-    <div className='rounded-lg flex flex-col gap-2 p-2 shadow-lg hover:shadow-2xl cursor-pointer hover:translate-y-[-5px] transition-all duration-300'>
-        <img src={item.item.image} alt="product image" className=''/>
+    <div className='rounded-lg flex flex-col gap-2 p-2 shadow-lg hover:shadow-2xl  hover:translate-y-[-5px] transition-all duration-300'>
+        <img src={item.item.image} alt="product image" className='cursor-pointer'/>
         <h2 className='font-bold text-xl py-2'>{item.item.name}</h2>
         <p className='text-gray-600 text-[15px] min-h-20'>{item.item.description}</p>
         <div className='flex'>
             <div className='flex flex-col w-1/3 gap-2'>
-                <span >Size</span>
-                <select name="size" className='w-1/2 outline-1 outline-gray-500 rounded-md'>
+                <span className="">Size</span>
+                <select name="size" className='w-1/2 outline-1 outline-gray-500 rounded-md bg-black text-white'>
                     {
                         item.item.sizes.map((item,index)=>(
-                            <option value={item} key={index} className="text-center">{item}</option>
+                            <option value={item} key={index} className="text-center bg-black text-white curpo">{item}</option>
                         ))
                     }
                 </select>
@@ -33,11 +34,11 @@ const ProductCard = (item) => {
             </div>
         </div>
         
-        <div className='flex py-2 justify-between'>
+        <div className='flex py-2 justify-between '>
             <span className='font-bold text-xl'>$ {item.item.price}</span>
-            <div className="flex gap-2 mr-4 border border-gray-400 shadow-md rounded-md px-4 items-center ">
+            <div className="hover:bg-black hover:text-white transition-all duration-300 hover:cursor-pointer  flex gap-2 mr-4 border border-gray-400 shadow-md rounded-md px-4 items-center ">
                 <IoCartOutline></IoCartOutline>
-                <button className='font-semibold'>Add to Cart</button>
+                <button className='font-semibold hover:cursor-pointer'>Add to Cart</button>
             </div>
         </div>
         
