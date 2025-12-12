3e66c43b6633587802baeaa313c4ac435d01a79c
huywb <pthhuy1306@gmail.com>
2025-12-12 16:40:16 +0700

    ListProduct and MenuItem

--- frontend/src/components/ListProducts.jsx
+++ frontend/src/components/ListProducts.jsx
@@ -1,11 +1,15 @@
+import { useEffect, useState } from 'react'
 import { ProductList } from '../contants/Data'
 import ProductCard from './ProductCard'
+import { useLocation } from 'react-router-dom'
 
-const ListProducts = () => {
+const ListProducts = ({productList}) => {
+  const pathname = useLocation()
+
   return (
     <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4  gap-4'>
         {
-            ProductList.map((item,index)=>(
+            productList.map((item,index)=>( 
                 <ProductCard item={item}/>
             ))
         }
--- frontend/src/components/MenuItem.jsx
+++ frontend/src/components/MenuItem.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { MenuList } from '../contants/Data'
 import { Link, useLocation } from 'react-router-dom'
 
-const MenuItem = () => {
+const MenuItem = ({changeCategory}) => {
     const pathname = useLocation()
     console.log(pathname.pathname.includes())
   return (
     <div className='bg-gray-300 p-2 grid grid-cols-2 gap-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-8 w-full place-items-center'>
       {
         MenuList.map((item,index)=>(
-            <Link to={`/products/${item.name}`} key={index} className={` flex gap-2 items-center w-full  justify-center ${pathname.pathname.includes(item.name)  ? 'font-semibold bg-gray-100' : ''}`}>
+            <Link onClick={()=>changeCategory(item.name)} to={`/products/${item.name}`} key={index} className={` flex gap-2 items-center w-full  justify-center ${pathname.pathname.includes(item.name)  ? 'font-semibold bg-gray-100' : ''}`}>
                     {item.icon}
                     <span >{item.name}</span>
             </Link>
