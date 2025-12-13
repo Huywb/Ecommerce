db74ac04924cc59cf99c4b31889d15c4420096d4
huywb <pthhuy1306@gmail.com>
2025-12-13 17:05:06 +0700

    CartDetails component

---
+++ frontend/src/components/CartDetails.jsx
@@ -0,0 +1,45 @@
+import { FaArrowRight } from "react-icons/fa";
+import { ProductCart } from "../contants/Data";
+             
+const CartDetails = ({handleChangeStatus}) => {
+    let discount = -10
+    let fee = 10
+    let Subtotal = ProductCart.reduce((sum,item)=>(sum += item.price * item.quantity),0)
+
+    const handleContinue = ()=>{
+        handleChangeStatus(2)
+    }
+
+  return (
+    <div className='flex flex-col gap-2 border border-gray-200 shadow-xl w-full p-4 pl-8'>
+        <h1 className="font-bold text-xl">Cart Details</h1>
+        <div className='flex flex-col gap-2 pt-4'>
+            <div className='flex gap-4 justify-between' >
+                <span className="text-gray-600">Subtotal</span>
+                <p className="font-bold">${Subtotal}</p>
+            </div>
+            <div className='flex gap-2 justify-between'>
+                <span className="text-gray-600">Discount</span>
+                <p className="font-bold">${discount}</p>
+            </div>
+            <div className='flex gap-2 justify-between '>
+                <span className="text-gray-600">Shipping Fee</span>
+                <p className="font-bold">${fee}</p>
+            </div>
+        </div>
+
+        <span className="border-b-2 border-gray-200 pt-2"></span>
+        
+        <div className='flex gap-2 py-4 justify-between font-bold'>
+            <span>Total</span>
+            <p>{Subtotal + fee + discount}</p>
+        </div>
+
+        <button onClick={handleContinue} className='w-full duration-300 transition-all cursor-pointer flex gap-2 items-center justify-center p-2 bg-gray-800 rounded-md hover:bg-gray-900 text-white'>
+            Continue <FaArrowRight />
+        </button>
+    </div>
+  )
+}
+
+export default CartDetails
