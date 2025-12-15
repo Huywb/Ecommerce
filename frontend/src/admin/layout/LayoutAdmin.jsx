603da355fad301dcfb406d82080e4decbba480ca
huywb <pthhuy1306@gmail.com>
2025-12-15 22:19:40 +0700

    Layout admin page

---
+++ frontend/src/admin/layout/LayoutAdmin.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+import HeaderAdmin from '../components/HeaderAdmin'
+import SiderBarAdmin from '../components/SiderBarAdmin'
+
+const LayoutAdmin = () => {
+    const [hideSiderBar,setHideSiderBar] = useState(false)
+    const ToggleHideSiderBar = ()=>{
+        setHideSiderBar(!hideSiderBar)
+    }
+    console.log(hideSiderBar)
+  return (
+    <div className='flex gap-4 bg-[#0b0b0b] h-screen'>
+        <section className={`flex w-64 bg-[#171717] transition-all duration-300 ease-in-out ${hideSiderBar ? 'mx-4 w-[56px] overflow-hidden' : ''}`}>
+            <SiderBarAdmin  hideSiderBar={hideSiderBar}/>
+        </section>
+        <section className={`flex w-5/6 p-4  `}>
+            <div className='flex flex-col w-full'>
+                <HeaderAdmin ToggleHideSiderBar={ToggleHideSiderBar} hideSiderBar={hideSiderBar} />
+                <Outlet></Outlet>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default LayoutAdmin
