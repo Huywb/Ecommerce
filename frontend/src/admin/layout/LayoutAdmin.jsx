f0e7282d718397b1561368aa120a7533bc5927ba
huywb <pthhuy1306@gmail.com>
2025-12-18 19:07:51 +0700

    Fix Error position Layoutadmin

--- frontend/src/admin/layout/LayoutAdmin.jsx
+++ frontend/src/admin/layout/LayoutAdmin.jsx
@@ -4,22 +4,35 @@ import HeaderAdmin from '../components/HeaderAdmin'
 import SiderBarAdmin from '../components/SiderBarAdmin'
 
 const LayoutAdmin = () => {
-    const [hideSiderBar,setHideSiderBar] = useState(false)
-    const ToggleHideSiderBar = ()=>{
-        setHideSiderBar(!hideSiderBar)
-    }
-    console.log(hideSiderBar)
+  const [hideSiderBar, setHideSiderBar] = useState(false)
+
+  const ToggleHideSiderBar = () => {
+    setHideSiderBar(!hideSiderBar)
+  }
+
   return (
-    <div className='flex gap-4 bg-[#0b0b0b] h-screen'>
-        <section className={`flex w-64 bg-[#171717] transition-all duration-300 ease-in-out ${hideSiderBar ? 'mx-4 w-[50px] overflow-hidden' : ''}`}>
-            <SiderBarAdmin  hideSiderBar={hideSiderBar}/>
-        </section>
-        <section className={`flex w-5/6 p-4  ${hideSiderBar ? 'w-full' : ''}`}>
-            <div className='flex flex-col w-full'>
-                <HeaderAdmin ToggleHideSiderBar={ToggleHideSiderBar} hideSiderBar={hideSiderBar} />
-                <Outlet></Outlet>
-            </div>
-        </section>
+    <div className="min-h-screen bg-[#0b0b0b]">
+      <section
+        className={`fixed top-0 left-0 h-screen bg-[#171717] transition-all duration-300 ease-in-out z-50
+          ${hideSiderBar ? 'w-[50px]' : 'w-64'}
+        `}
+      >
+        <SiderBarAdmin hideSiderBar={hideSiderBar} />
+      </section>
+
+      <section
+        className={`transition-all duration-300
+          ${hideSiderBar ? 'ml-[50px]' : 'ml-64'}
+        `}
+      >
+        <div className="flex flex-col min-h-screen p-4">
+            <HeaderAdmin
+            ToggleHideSiderBar={ToggleHideSiderBar}
+            hideSiderBar={hideSiderBar}
+          />
+          <Outlet />
+        </div>
+      </section>
     </div>
   )
 }
