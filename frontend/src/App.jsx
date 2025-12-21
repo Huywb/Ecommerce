eab9111b9e1c3b3623fb6153351bd71f2e6ac4a5
huywb <pthhuy1306@gmail.com>
2025-12-21 15:25:12 +0700

    Update route and mock Data for Admin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,11 @@ import Register from "./pages/Register";
 import Products from "./pages/Products";
 import ProductDetail from "./components/product/ProductDetail";
 import Cart from "./pages/Cart";
+import AdminHome from "./admin/pages/AdminHome";
+import LayoutAdmin from "./admin/layout/LayoutAdmin";
+import AdminUsers from "./admin/pages/AdminUsers";
+import AdminProducts from "./admin/pages/AdminProducts";
+import AdminOrders from "./admin/pages/AdminOrders";
 
 function App() {
   const router = createBrowserRouter([
@@ -32,6 +37,28 @@ function App() {
         {
           path: "cart",
           element: <Cart />
+        }
+      ],
+    },
+    {
+      path: '/admin',
+      element: <LayoutAdmin />,
+      children: [
+        {
+          index: true,
+          element: <AdminHome />
+        },
+        {
+          path: "users",
+          element: <AdminUsers />
+        },
+        {
+          path: "products",
+          element: <AdminProducts />
+        },
+        {
+          path: "orders",
+          element: <AdminOrders />
         }
       ]
     }
