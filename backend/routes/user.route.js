b5661f09e412bad4bbe728155c54f2fce3ef85ad
huywb <pthhuy1306@gmail.com>
2025-12-29 22:13:22 +0700

    User route set up

---
+++ backend/routes/user.route.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { DeleteUserById, GetAllUsers, GetUserById, Login, Register, UpdateUserById } from '../controller/v1/User.controller';
+import { validateAccessToken } from '../middleware/GenerationToken';
+
+
+const router = express.Router();
+
+router.post('/login', Login)
+router.post('/register', Register)
+router.get('/all', validateAccessToken, GetAllUsers)
+router.get('/:id', validateAccessToken, GetUserById)
+router.patch('/update/:id', validateAccessToken, UpdateUserById)
+router.delete('/delete/:id', validateAccessToken, DeleteUserById)
+export default router;
