2fb42aca551c2a2da74d23668acc1a0c9737af85
huywb <pthhuy1306@gmail.com>
2026-01-04 16:49:04 +0700

    Server setup for all route api

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import dotenv from 'dotenv'
 import bodyParser from 'body-parser';
 import { connectDB } from './libs/ConnectDB.js';
 import userRouter from './routes/user.route.js';
+import productRouter from './routes/product.route.js';
+import bannerRouter from './routes/banner.route.js';
+import categoryRouter from './routes/category.route.js';
+import cartRouter from './routes/cart.route.js';
+import orderRouter from './routes/order.route.js';
 
 
 dotenv.config() 
@@ -17,6 +22,12 @@ connectDB();
 
 
 app.use('/api/v1/user', userRouter);
+app.use('/api/v1/product', productRouter)
+app.use('/api/v1/banner',bannerRouter)
+app.use('/api/v1/category',categoryRouter)
+app.use('/api/v1/logo',logoRouter)
+app.use('/api/v1/cart',cartRouter)
+app.use('/api/v1/order',orderRouter)
 
 
 
