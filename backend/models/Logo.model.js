dbb9d8801f4d9b5934378a70ab0565a55c5b3908
Huywb <pthhuy1306@gmail.com>
2026-01-06 13:43:49 +0700

    Setup middleware checkrole (#6)

--- backend/models/Logo.model.js
+++ backend/models/Logo.model.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const logoSchema = new mongoose.Schema(
     {
         imageUrl: { type: String, required: true },
+        active: {type: Boolean,default: false}
     },
     { timestamps: true }
 );
