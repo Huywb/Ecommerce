7b104233d37786fb98a06a5123c37abe3a2bcd2b
huywb <pthhuy1306@gmail.com>
2026-01-06 13:42:59 +0700

    Setup middleware checkrole

--- backend/middleware/GenerationToken.js
+++ backend/middleware/GenerationToken.js
@@ -1,11 +1,12 @@
 import jwt from 'jsonwebtoken'
 
-export const generateAccessToken = (id) => {
+export const generateAccessToken = (data) => {
     try {
-        const token = jwt.sign({id},process.env.JWT_SECRET,{expiresIn:'1d'})
+        const token = jwt.sign({id:data._id,role:data.role},process.env.JWT_SECRET,{expiresIn:'1d'})
         return token
     } catch (error) {
         console.log(error)
+        res.status(401).json({message:"Unauthorized",status:false})
     }
 }
 
@@ -18,10 +19,20 @@ export const validateAccessToken = (req,res,next)=>{
         const token = authHeader.split(" ")[1]
         const decoded = jwt.verify(token,process.env.JWT_SECRET)
         req.userId = decoded.id
+        req.userRole = decoded.role
         next()
         
     } catch (error) {
         console.log(error)
         res.status(401).json({message:"Unauthorized",status:false})
+    }
+}
+
+export const checkRole = (...role)=>{
+    return (req,res,next)=>{
+        if(!req.userRole !== !role.includes(req.user.role)){
+            return res.status(403).json({message:"Forbidden: You do not have permission",status:false})
+        }
+        next()
     }
 }
