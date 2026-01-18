bd784d3aa7c00b0b2329df60335174b08778ab46
huywb <pthhuy1306@gmail.com>
2026-01-18 19:52:31 +0700

    Fix/user-controller-logic

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { DeleteUserById, GetAllUsers, GetUserById, Login, Register, UpdateUserById } from '../controller/v1/User.controller';
-import { validateAccessToken } from '../middleware/GenerationToken';
+import { AddUser, DeleteUserById, GetAllUsers, GetUserById, Login, Register, UpdateUserById } from '../controller/v1/User.controller.js';
+import { validateAccessToken } from '../middleware/GenerationToken.js';
+import upload from '../config/multer.js';
 
 
 const router = express.Router();
@@ -9,7 +10,8 @@ router.post('/login', Login)
 router.post('/register', Register)
 router.get('/all', validateAccessToken, GetAllUsers)
 router.get('/:id', validateAccessToken, GetUserById)
-router.patch('/update/:id', validateAccessToken, UpdateUserById)
+router.patch('/update/:id', validateAccessToken,upload.single('file'), UpdateUserById)
 router.delete('/delete/:id', validateAccessToken, DeleteUserById)
+router.post('/addUser',validateAccessToken,upload.single('file'),AddUser)
 
 export default router;
